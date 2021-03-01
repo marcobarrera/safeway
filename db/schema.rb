30f3f2a9e964fc80b0ed03b61d5bf57d856e7d21
Marianna Askova <marianna.askova@gmail.com>
2021-03-01 15:52:34 -0600

    add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_213528) do
+ActiveRecord::Schema.define(version: 2021_03_01_215038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 2021_03_01_213528) do
     t.index ["user_id"], name: "index_alerts_on_user_id"
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "email"
+    t.boolean "emergency_contact"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 2021_03_01_213528) do
   end
 
   add_foreign_key "alerts", "users"
+  add_foreign_key "contacts", "users"
 end
