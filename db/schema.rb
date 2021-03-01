7097d4c45b828b0c365da2c04c3066ab2e90929c
Marco Barrera <marcobarrera749@gmail.com>
2021-03-01 14:12:02 -0800

    Added address dependency to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_01_215609) do
-
-
-ActiveRecord::Schema.define(version: 2021_03_01_215038) do
-
-ActiveRecord::Schema.define(version: 2021_03_01_212755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: :cascade do |t|
+    t.string "address"
+    t.string "label"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "alerts", force: :cascade do |t|
     t.string "category"
@@ -44,16 +48,6 @@ ActiveRecord::Schema.define(version: 2021_03_01_212755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contacts_on_user_id"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "address"
-    t.string "label"
-    t.float "latitude"
-    t.float "longitude"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,10 +68,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_212755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "addresses", "users"
   add_foreign_key "alerts", "users"
   add_foreign_key "contacts", "users"
-
-  add_foreign_key "addresses", "users"
-
 end
