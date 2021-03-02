9735d87776ae134ccc753d3641a36854d27c3d61
Marco Barrera <marcobarrera749@gmail.com>
2021-03-02 10:17:38 -0800

    Created alerts controller

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,21 @@
+class AlertsController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def notify
+
+  end
+
+  def share_location
+
+  end
+end
