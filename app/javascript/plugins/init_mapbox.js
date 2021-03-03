94020514df881a96de9f1e2518ef846d7a6ead4b
Marianna Askova <marianna.askova@gmail.com>
2021-03-03 15:15:46 -0600

    added if statment for different markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -31,12 +31,22 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
-      new mapboxgl.Marker()
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+        // Create a HTML element for your custom marker
+        const element = document.createElement('div');
+        element.className = 'marker';
+        element.style.backgroundImage = `url('${marker.image_url}')`;
+        element.style.backgroundSize = 'contain';
+        element.style.width = '25px';
+        element.style.height = '25px';
+      new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
     });
 
-    addMarkersToMap(map, markers);
+    // addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
