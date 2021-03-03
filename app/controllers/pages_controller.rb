eb8bfa9d8ddf15420d41d594b081c75cb341c5b0
Marianna Askova <marianna.askova@gmail.com>
2021-03-03 11:15:29 -0600

    Add map config

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @alerts = Alert.all
+    @markers = @alerts.geocoded.map do |alert|
+      {
+        lat: alert.latitude,
+        lng: alert.longitude
+      }
+    end
   end
 end
