8dda610136ac0bd1b56d0e6c34ed924bd2f1244b
teresitasachsen <teresitasachsen99@gmail.com>
2021-03-03 12:24:06 -0600

    address controler complete and created new view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :contacts, only: [:create, :index, :destroy, :update]
-  resources :addresses, only: [:create, :index, :destroy, :update]
+  resources :addresses, only: [:new, :create, :index, :destroy, :update]
   resources :alerts, only: [:create, :index, :update] do
     collection do
       post 'notify' => 'alerts#notify', :as => :notify
