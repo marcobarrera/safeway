53dc3154dab3d48267850d5d222b59a4fc481855
Marianna Askova <marianna.askova@gmail.com>
2021-03-05 11:04:50 -0600

    mid-commit to pull master

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -19,13 +19,13 @@ class AlertsController < ApplicationController
     client = Twilio::REST::Client.new(account_sid, auth_token)
     emergency_contacts = current_user.contacts.where(emergency_contact: true)
     from = '+14088316357' # Your Twilio number
-    emergency_contacts.each do |contact|
+    # emergency_contacts.each do |contact|
       client.messages.create(
         from: from,
-        to: contact.phone_number,
+        to: '+12016879114',
         body: "Hey friend! I'm in trouble. My location is"
       )
-    end
+    # end
   end
 
   def share_location
