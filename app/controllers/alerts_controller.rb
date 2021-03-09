75582945d4f450677da638d26e05815556d071e1
Marco Barrera <marcobarrera749@gmail.com>
2021-03-09 07:46:44 -0800

    Fixed formatting

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
 class AlertsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[share_location notify]
@@ -24,16 +24,16 @@ class AlertsController < ApplicationController
   end
 
   def notify
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
     client = Twilio::REST::Client.new(account_sid, auth_token)
     emergency_contacts = current_user.contacts.where(emergency_contact: true)
-    from = '+14088316357' # Your Twilio number
+    from = "+14088316357" # Your Twilio number
     emergency_contacts.each do |contact|
       client.messages.create(
         from: from,
         to: contact.phone_number,
-        body: "Hey friend! I'm in trouble. My location is http://www.google.com/maps/place/#{params['myLat']},#{params['myLng']}"
+        body: "Hey friend! I'm in trouble. My location is http://www.google.com/maps/place/#{params["myLat"]},#{params["myLng"]}",
       )
     end
     Alert.create!(longitude: params[:myLng], latitude: params[:myLat], user_id: current_user.id)
