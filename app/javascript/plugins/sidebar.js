e4a0c78c6d5ad4a95e82a23eda5650bdfd13e6f3
JuanCabrera <jlcabrera333@gmail.com>
2021-03-12 13:25:10 -0700

    shield icon in create alert

---
+++ app/javascript/plugins/sidebar.js
@@ -0,0 +1,38 @@
+// const checkParams=() => {
+//   URLSearchParams(window.location.search);
+//     if (searchParams.get('modal') && searchParams.get('?')){
+//       $('#sidebarModal').modal('show')
+//      }
+//     end
+//   // const paramsString = "#sidebarModal"
+//   // const searchParams = new URLSearchParams(paramsString);
+
+//   // for (let p of searchParams) {
+//   // console.log(p);
+//   // }
+
+//   // searchParams.get("paramsString") === true;
+//   // const querystring = window.location.search
+//   // console.log(querystring)
+//                 // var modal=$('#sidebarModal');
+//         // $('#sidebar').click(function(e) {
+//         //   var edit_url = $(this).attr('href');
+//         //   modal.load(edit_url + ' #content',function(){
+//         //     modal.dialog("open");
+//         //   });
+//         // });
+//         // modal.dialog({ autoOpen: false, title: "Your title", draggable: true,
+//         // resizable: false, modal: true, width:'auto'});
+
+//     };
+
+
+// // const checkParams = () => {
+// //   // 1. Obtienes la URL actual
+// //   // 2. Revisas los params (query strings)
+// //   // 3. IF existe el param `modal=true`
+// //   // 4. entonces abres el modal
+// // }
+
+
+// export {checkParams};
